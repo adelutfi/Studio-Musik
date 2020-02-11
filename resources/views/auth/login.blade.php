62b0ac3d1ffdcf4ef4c0a6abef7829feab9bfe8f
royhan31 <izzapark34@gmail.com>
2020-02-11 11:00:43 +0700

    new login & register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,23 +11,25 @@
                             Login
                         </h2>
                         <div class="row justify-content-center">
-                <div class="col-lg-4">
-                    <div class="tabilContainer table-responsive">
-                    	 <div class="comment-form-area">
-                         <form action="blog-details.html" class="comments-entry-form">
-                            <div class="form-group">
-                                <input type="email" class="form-control" placeholder="Masukan Email anda">
-                            </div>
-                            <div class="form-group">
-                                <input type="password" class="form-control" placeholder="Masukan password anda">
-                            </div>
-                         
-                            <button type="submit" class="submit-btn">Login</button>
-                        </form>
-                    </div>
-                    </div>
-                </div>
-            </div>
+                          <div class="col-lg-4">
+                              <div class="tabilContainer table-responsive">
+                              	 <div class="comment-form-area">
+                                   <form action="" class="comments-entry-form">
+                                      <div class="form-group">
+                                          <label for=""><strong>Email</strong></label>
+                                          <input type="email" class="form-control" placeholder="Masukan Email anda">
+                                      </div>
+                                      <div class="form-group">
+                                            <label for=""><strong>Password</strong></label>
+                                          <input type="password" class="form-control" placeholder="Masukan password anda">
+                                      </div>
+
+                                      <button class="submit" type="submit">Login</button>
+                                  </form>
+                              </div>
+                              </div>
+                          </div>
+                      </div>
                     </div>
                 </div>
             </div>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -11,29 +11,33 @@
                             Register
                         </h2>
                         <div class="row justify-content-center">
-                <div class="col-lg-4">
-                    <div class="tabilContainer table-responsive">
-                    	 <div class="comment-form-area">
-                         <form action="blog-details.html" class="comments-entry-form">
-                            <div class="form-group">
-                                <input type="text" class="form-control" placeholder="Masukan Nama anda">
-                            </div>
-                            <div class="form-group">
-                                <input type="email" class="form-control" placeholder="Masukan Email anda">
-                            </div>
-                            <div class="form-group">
-                                <input type="password" class="form-control" placeholder="Masukan password anda">
-                            </div>
-                            <div class="form-group">
-                                <input type="password" class="form-control" placeholder="Konfirmasi Password anda">
-                            </div>
-                         
-                            <button type="submit" class="submit-btn">Register</button>
-                        </form>
-                    </div>
-                    </div>
-                </div>
-            </div>
+                          <div class="col-lg-4">
+                              <div class="tabilContainer table-responsive">
+                              	 <div class="comment-form-area">
+                                   <form action="blog-details.html" class="comments-entry-form">
+                                      <div class="form-group">
+                                          <label for="">Nama</label>
+                                          <input type="text" class="form-control" placeholder="Masukan Nama anda">
+                                      </div>
+                                      <div class="form-group">
+                                          <label for="">Email</label>
+                                          <input type="email" class="form-control" placeholder="Masukan Email anda">
+                                      </div>
+                                      <div class="form-group">
+                                            <label for="">Password</label>
+                                          <input type="password" class="form-control" placeholder="Masukan password anda">
+                                      </div>
+                                      <div class="form-group">
+                                          <label for="">Ulangi Password</label>
+                                          <input type="password" class="form-control" placeholder="Konfirmasi Password anda">
+                                      </div>
+
+                                        <button class="submit" type="submit">Register</button>
+                                  </form>
+                              </div>
+                              </div>
+                          </div>
+                      </div>
                     </div>
                 </div>
             </div>
