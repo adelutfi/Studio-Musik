b90ffd0e379331b449cccaf4e1f5513040e44e97
royhan31 <izzapark34@gmail.com>
2020-02-29 15:48:07 +0700

    production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Carbon;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +25,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+      config(['app.locale' => 'id']);
+      Carbon::setLocale('id');
+      date_default_timezone_set('Asia/Jakarta');
+      Schema::defaultStringLength(191);
+      if(config('app.env') === 'production') {
+          \URL::forceScheme('https');
+      }
     }
 }
