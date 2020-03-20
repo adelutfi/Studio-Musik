6d8e20758588d5cac559c869f43ec5b7131aa342
royhan31 <izzapark34@gmail.com>
2020-03-21 06:07:27 +0700

    studio

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -19,8 +19,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\TrustProxies::class,
-        \Barryvdh\Cors\HandleCors::class,
-        \App\Http\Middleware\Cors::class,
+        \Fruitcake\Cors\HandleCors::class,
     ];
 
     /**
@@ -42,7 +41,6 @@ class Kernel extends HttpKernel
         'api' => [
             'throttle:60,1',
             'bindings',
-            \Barryvdh\Cors\HandleCors::class,
         ],
     ];
 
