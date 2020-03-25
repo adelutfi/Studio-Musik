3237559424f7750fe50c44bef53baba843ee56b8
royhan31 <izzapark34@gmail.com>
2020-03-25 07:16:53 +0700

    studio detail

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -31,6 +31,7 @@ class AppServiceProvider extends ServiceProvider
       Schema::defaultStringLength(191);
       if(config('app.env') === 'production') {
           \URL::forceScheme('https');
+          $this->app['request']->server->set('HTTPS', true);
       }
     }
 }
