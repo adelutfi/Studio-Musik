5daf123754eb56aac1108fd8962b7caef6b2aeeb
royhan31 <izzapark34@gmail.com>
2020-03-25 07:31:45 +0700

    studio detail

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends HttpKernel
      */
     protected $middleware = [
         \App\Http\Middleware\CheckForMaintenanceMode::class,
-        \App\Http\Middleware\Cors::class,
+        \App\Http\Middleware\HttpsProtocol::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
